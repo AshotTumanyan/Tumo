ca57cd163eb142b048d53716e023b36ea2dbf0bf
AshotTumanyan <ashot.tumanyan1@tumo.org>
2024-05-17 17:25:33 +0400

    final

--- Audio_Player_Ashot_Tumanyan/script.js
+++ Audio_Player_Ashot_Tumanyan/script.js
@@ -5,20 +5,35 @@ let data = {
         "BIBI PHONK BR",
         "Kerosene",
         "Daylight",
-    ],
-    song: [
+        "murder in my mind",
+        "Asphalt 8",
+        "Neon Blade",
+        "Life In Rio",
+        "Suicide Year",
+            ],
+    song:   [
         "music/Ace Of Base - Happy Nation.mp3",
         "music/music2.mp3",
         "music/Bibi Babydoll  DJ FKU - BIBI PHONK BR.mp3",
         "music/Crystal Castles - Kerosene.mp3",
         "music/David Kushner - Daylight.mp3",
-    ],
+        "music/KORDHELL-Murder-In-My-Mind.mp3",
+        "music/Macan - ASPHALT 8.mp3",
+        "music/neon-blade-moondeity-[edit-audio]-made-with-Voicemod.mp3",
+        "music/Slowboy, NUEKI, Crazy Mano feat. TOLCHONOV - Life in Rio.mp3",
+        "music/WEEDMANE - SUICIDE YEAR.mp3",
+            ],
     poster: [
         "https://upload.wikimedia.org/wikipedia/en/4/4b/Happy_nation.jpg",
         "https://miro.medium.com/v2/resize:fit:1400/1*XrnXihETfw2zHC-T5U5L7Q.jpeg",
         "https://i.ytimg.com/vi/HttkgipRz9Q/maxresdefault.jpg",
         "https://i.ytimg.com/vi/_GSoMTdSeVo/sddefault.jpg",
-        "https://i.cbc.ca/1.3474686.1457036658!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/sunset-silhouette-germany-weather-spring.jpg"
+        "https://i.cbc.ca/1.3474686.1457036658!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/sunset-silhouette-germany-weather-spring.jpg",
+        "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3b3d6fd1-28cc-4b06-adaa-5052d94d3070/dg1g150-7a64b170-c658-4e06-b5cd-96d3f533351c.png/v1/fill/w_1280,h_727,q_80,strp/murder_in_my_mind_by_gwensenpaii_dg1g150-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzI3IiwicGF0aCI6IlwvZlwvM2IzZDZmZDEtMjhjYy00YjA2LWFkYWEtNTA1MmQ5NGQzMDcwXC9kZzFnMTUwLTdhNjRiMTcwLWM2NTgtNGUwNi1iNWNkLTk2ZDNmNTMzMzUxYy5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.ZO99mtjEY3mzeA_DGp-3PV0RGYHs_yAA_9G5BdzvKjc",
+        "https://i1.sndcdn.com/artworks-sBKtY1HEu8HzbFdm-PhcDgA-t500x500.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9wEs9ojUiwOTYFhgokBqPCPhDF9LdeV_4fmXfDwZ4Vg&s",
+        "https://i.ytimg.com/vi/7crimQ3nhGw/maxresdefault.jpg",
+        "https://i.ytimg.com/vi/4dh06N68aOY/hqdefault.jpg",
     ],
 }
 
@@ -91,3 +106,31 @@ function pre() {
     }
     playSong()
 }
+
+function mute(){
+    let mute = document.getElementById("mute")
+    if(song.muted){
+        song.muted = false
+        mute.src = "images/volume.png"
+    }
+    else {
+        song.muted = true
+        mute.src = "images/volume-mute.png"
+    }
+}
+function decrease(){
+    song.volume-= 0.2
+    // if(song.volume <= 0.2){
+    //     song.volume = 0
+    //     mute.src = "images/volume.png"
+    // }
+}
+
+function increase(){
+    song.volume += 0.2
+    // if(song.volume > 2){
+    //     song.volume = 2
+    //     mute.src = "images/volume.png"
+    // }
+}
+
